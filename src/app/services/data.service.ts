8c6f3452fe54f570022d1e8cfaac3fa8d97cba9e
Leedabord <gwforumlakers@gmail.com>
2022-06-26 21:31:58 -0700

    1 file modified

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Contact } from '../models/contact';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, BehaviorSubject, of, throwError } from 'rxjs';
 import { tap, map, retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
+  private message = new BehaviorSubject('First Message');
+  sharedMessage = this.message.asObservable();
 
   public aaposts: [];
   public status = "null";
@@ -26,6 +29,10 @@ export class DataService {
   private lastId: number = 20;
 
   constructor(private httpC: HttpClient) { }
+
+  nextMessage(message: string) {
+    this.message.next(message)
+  }
 
   public rdbGet(): Observable<any> {
     return this.httpC.get<any>(this.rdbURL, this.httpOptions);
