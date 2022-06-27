427a448dca42787535c963d394f3d6a413070e3d
Leedabord <gwforumlakers@gmail.com>
2022-06-26 19:33:26 -0700

    1 file modified

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { DataService } from '../services/data.service';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    DataService,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
